dd9edf7bae0995dbe828496a5cd6edee289bd380
EduardoColombari <duducolombarielias@gmail.com>
2025-02-20 21:44:31 -0300

    exercicio modelo OO em Java

---
+++ src/Carro.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+
+public class Carro {
+    public int ano;
+    public float velocidade;
+    public String marca;
+    public String modelo;
+    public  String mensagem;
+
+    //metodo contrutor
+    public  Carro(int ano, float velocidade, String marca, String modelo){
+        this.ano =  ano;
+        this.velocidade =  velocidade;
+        this.marca = marca;
+        this.modelo =  modelo;
+
+    }
+
+    public  Carro(){
+        this.marca =  "Vazia";
+        this.modelo =  "Vazio";
+
+
+    }
+
+
+
+//    this represnta o objeto que chama o método
+    public void exibirDetalhes() {
+        if(this.velocidade < 0) {
+            JOptionPane.showMessageDialog(null,"A velocidade não pode ficar negativa");
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"\nMarca: " + this.marca
+                    + " \nModelo: " + this.modelo
+                    + " \nAno: " + this.ano
+                    + " \nVelocidade: "  + this.velocidade);
+        }
+    }
+
+    public void acelerar(float x){
+        this.velocidade = this.velocidade + x;
+    }
+
+    public void frear(float y){
+        if(this.velocidade - y >= 0) {
+            this.velocidade -= y;
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"A velocidade não pode ficar negativa");
+        }
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+
+        Carro obj1 = new Carro(2015, 30.0F , "Chevrolet","Onix");
+        obj1.exibirDetalhes();
+        obj1.acelerar(60.0F);
+        obj1.frear(100);
+        obj1.exibirDetalhes();
+
+        Carro obj2 = new Carro();
+        obj2.ano = 2018;
+        obj2.velocidade = 0.0F;
+        obj2.marca = "Volks";
+        obj2.modelo = "Gol";
+        obj2.exibirDetalhes();
+        obj2.acelerar(50.0F);
+        obj2.exibirDetalhes();
+
+        Carro obj3 = new  Carro();
+        obj3.exibirDetalhes();
+
+
+    }
+}
